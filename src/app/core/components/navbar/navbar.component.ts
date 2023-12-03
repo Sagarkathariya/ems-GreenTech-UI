32d15e8a059ae88826fa5723355964bc5c4f18e8
sagarkathariya <sagarkathariya@gmail.com>
2023-12-03 21:50:06 +0545

    tttt

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
 
+export class NavbarComponent  {
+public ImagePath = "/assets/ems.png";
 }
+
+
+//export class NavbarComponent implements  OnInit {
+  // public ImagePath = "/assets/ems.png";
+  //ImagePath:string;
+
+  //constructor(){
+  //   this.ImagePath = "/assets/ems.png";
+  // }
+  // constructor() { 
+  //   //image location 
+  //   this.ImagePath = 'src\ems.png';
+  // }
+  
+//   } 
+//}
---
+++ src/app/features/category/services/category.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http:HttpClient) { }
+
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    // Add your implementation here
+    return this.http.post<void>('https://localhost:44383/api/categories', model);
+  }
+
+  getCategoryList(): Observable<void> {
+    // Add your implementation here
+    return this.http.get<void>('https://jsonplaceholder.typicode.com/todos');
+  }
+
+}
